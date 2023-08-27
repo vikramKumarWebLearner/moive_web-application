99ddb48899434973c11ced59b5f528209d16fa22
vikram <vikrampchoudhary777@gmail.com>
2023-08-27 10:24:17 +0530

    people component add

--- src/pages/people/People.jsx
+++ src/pages/people/People.jsx
@@ -1,10 +1,93 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+import "./style.scss";
+
+import { fetchDataFromApi } from "../../utils/api";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+// import MovieCard from "../../components/movieCard/MovieCard";
+import PeoplePoster from "../../components/people/People";
+import Spinner from "../../components/spinner/Spinner";
+// import noResults from "../../assets/no-results.png";
 
 const People = () => {
-  return (
-    <div>
-      <h1>Hello People</h1>
-    </div>
-  )
-}
+    const [data, setData] = useState(null);
+    const [pageNum, setPageNum] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const { query } = useParams();
 
-export default People
+    const fetchInitialData = () => {
+        setLoading(true);
+        fetchDataFromApi(`/trending/person/day`).then(
+            (res) => {
+                setData(res);
+                setPageNum((prev) => prev + 1);
+                setLoading(false);
+               
+            }
+           
+        );
+    };
+    // person/day?language=en-US
+    const fetchNextPageData = () => {
+        fetchDataFromApi(`/trending/person/day`).then(
+            (res) => {
+                if (data?.results) {
+                    setData({
+                        ...data,
+                        results: [...data?.results, ...res.results],
+                    });
+                    console.log(data)
+                } else {
+                    setData(res);
+                }
+                setPageNum((prev) => prev + 1);
+            }
+        );
+    };
+
+    useEffect(() => {
+        setPageNum(1);
+        fetchInitialData();
+    }, [query]);
+    return (
+        <div className="searchResultsPage">
+            {loading && <Spinner initial={true} />}
+            {!loading && (
+                <ContentWrapper>
+                    {data?.results?.length > 0 ? (
+                        <>
+                            <div className="pageTitle">
+                                {`trending People today`}
+                            </div>
+                            <InfiniteScroll
+                                className="content"
+                                dataLength={data?.results?.length || []}
+                                next={fetchNextPageData}
+                                hasMore={pageNum <= data?.total_pages}
+                                loader={<Spinner />}
+                            >
+                                {data?.results.map((item, index) => {
+                                    return (
+                                        <PeoplePoster
+                                            key={index}
+                                            data={item}
+                                            fromSearch={true}
+                                        />
+                                    );
+                                })}
+                            </InfiniteScroll>
+                        </>
+                    ) : (
+                        <span className="resultNotFound">
+                            Sorry, Results not found!
+                        </span>
+                    )}
+                </ContentWrapper>
+            )}
+        </div>
+    );
+};
+
+export default People;
